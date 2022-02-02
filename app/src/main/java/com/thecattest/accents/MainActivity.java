929b1f2522362b0f49aa84755fff27f57d1d32e5
thecattest <vodopyanov999@gmail.com>
2022-02-02 12:53:12 +0300

    mistakes now matter

--- app/src/main/java/com/thecattest/accents/MainActivity.java
+++ app/src/main/java/com/thecattest/accents/MainActivity.java
@@ -4,16 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.util.Log;
 import android.util.TypedValue;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout wordPlaceholder;
     private TextView commentPlaceholder;
     private ConstraintLayout root;
+    private SharedPreferences sharedPref;
+
+    private final String MISTAKES_FILENAME = "mistakes.json";
 
     private final LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
@@ -36,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         wordPlaceholder = findViewById(R.id.wordPlaceholder);
         commentPlaceholder = findViewById(R.id.extra);
         root = findViewById(R.id.root);
+        sharedPref = getPreferences(Context.MODE_PRIVATE);
 
         next();
     }
@@ -43,21 +60,68 @@ public class MainActivity extends AppCompatActivity {
     private void next() {
         commentPlaceholder.setText("");
         wordPlaceholder.removeAllViews();
-        String word = words.get((int)(Math.random() * words.size()));
-        if (word.contains("(")) {
-            String[] parts = word.split("\\(");
+
+        String fullWord = getNextWord();
+        String comment = "";
+        String word = fullWord;
+        if (fullWord.contains("(")) {
+            String[] parts = fullWord.split("\\(");
             word = parts[0];
-            String comment = parts[1];
+            comment = parts[1];
             comment = comment.substring(0, comment.length() - 1);
-            commentPlaceholder.setText(comment);
         }
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            wordPlaceholder.addView(createTextView(c), layoutParams);
-        }
+        for (int i = 0; i < word.length(); i++)
+            wordPlaceholder.addView(createTextView(fullWord, word.charAt(i)), layoutParams);
+        commentPlaceholder.setText(comment);
     }
 
-    private TextView createTextView(char c) {
+    private HashMap<String, Integer> getMistakes() {
+        String jsonString = readFromFile();
+        Gson gson = new GsonBuilder().create();
+        HashMap<String, Integer> mistakes = gson.fromJson(
+                jsonString, new TypeToken<HashMap<String, Integer>>(){}.getType());
+        Log.d("MISTAKES READ", mistakes.toString());
+        return mistakes;
+    }
+
+    private void setMistakes(HashMap<String, Integer> mistakes) {
+        Log.d("MISTAKES WRITE", mistakes.toString());
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(mistakes);
+        writeToFile(jsonString);
+    }
+
+    private void mistakeInc(String mistake) {
+        HashMap<String, Integer> mistakes = getMistakes();
+        Integer count = mistakes.get(mistake);
+        count = count != null ? count : 2;
+        mistakes.put(mistake, count+1);
+        setMistakes(mistakes);
+    }
+
+    private void mistakeDec(String mistake) {
+        HashMap<String, Integer> mistakes = getMistakes();
+        Integer count = mistakes.get(mistake);
+        count = count != null ? count : 0;
+        if (count > 1)
+            mistakes.put(mistake, count-1);
+        else
+            mistakes.remove(mistake);
+        setMistakes(mistakes);
+    }
+
+    private String getNextWord() {
+        HashMap<String, Integer> mistakes = getMistakes();
+        if (mistakes.size() != 0 && Math.random() < .1) {
+            Object[] mistakesKeys = mistakes.keySet().toArray();
+            String mistake = String.valueOf(mistakesKeys[(int)(Math.random() * mistakesKeys.length)]);
+            mistakeDec(mistake);
+            return mistake;
+        }
+        return words.get((int)(Math.random() * words.size()));
+    }
+
+    private TextView createTextView(String word, char c) {
         TextView textView = new TextView(MainActivity.this);
         textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 50);
         textView.setPadding(2, 0, 2, 0);
@@ -70,6 +134,8 @@ public class MainActivity extends AppCompatActivity {
                 vibrate(Character.isUpperCase(c) ? 15 : 200);
                 if (Character.isUpperCase(c))
                     next();
+                else
+                    mistakeInc(word);
             });
         } else
             textView.setTextColor(getResources().getColor(R.color.white));
@@ -92,5 +158,45 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void writeToFile(String data) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(MISTAKES_FILENAME, Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e);
+        }
+    }
+
+
+    private String readFromFile() {
+        String ret = "{}";
+
+        try {
+            InputStream inputStream = openFileInput(MISTAKES_FILENAME);
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e);
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e);
+        }
+
+        return ret;
+    }
 
 }
