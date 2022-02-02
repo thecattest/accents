dad86e2b2f39cb167d03fc70a658c08b6476fd2a
thecattest <vodopyanov999@gmail.com>
2022-02-02 11:50:00 +0300

    vibration

--- app/src/main/java/com/thecattest/accents/MainActivity.java
+++ app/src/main/java/com/thecattest/accents/MainActivity.java
@@ -3,24 +3,24 @@ package com.thecattest.accents;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.graphics.drawable.TransitionDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.TypedValue;
-import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> words;
     private LinearLayout wordPlaceholder;
-    private TextView extra;
+    private TextView commentPlaceholder;
     private ConstraintLayout root;
 
     private final LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -32,22 +32,24 @@ public class MainActivity extends AppCompatActivity {
 
         JSONResourceReader reader = new JSONResourceReader(getResources(), R.raw.accents);
         words = reader.constructUsingGson();
+
         wordPlaceholder = findViewById(R.id.wordPlaceholder);
-        extra = findViewById(R.id.extra);
+        commentPlaceholder = findViewById(R.id.extra);
         root = findViewById(R.id.root);
 
         next();
     }
 
     private void next() {
-        extra.setText("");
+        commentPlaceholder.setText("");
         wordPlaceholder.removeAllViews();
         String word = words.get((int)(Math.random() * words.size()));
         if (word.contains("(")) {
-            String[] split;
-            split = word.split("\\(");
-            word = split[0];
-            extra.setText(split[1].substring(0, split[1].length() - 1));
+            String[] parts = word.split("\\(");
+            word = parts[0];
+            String comment = parts[1];
+            comment = comment.substring(0, comment.length() - 1);
+            commentPlaceholder.setText(comment);
         }
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
@@ -63,17 +65,31 @@ public class MainActivity extends AppCompatActivity {
 
         if ("аеиоуэюяы".contains(String.valueOf(c).toLowerCase(Locale.ROOT))) {
             textView.setTextColor(getResources().getColor(R.color.red));
-            textView.setOnClickListener(v -> {
-                int colorId = Character.isUpperCase(c) ? R.drawable.correct : R.drawable.incorrect;
-                TransitionDrawable transition = (TransitionDrawable) getResources().getDrawable(colorId);
-                root.setBackground(transition);
-                transition.startTransition(400);
-                next();
+            textView.setOnClickListener(view -> {
+                animateBackground(Character.isUpperCase(c) ? R.drawable.correct : R.drawable.incorrect);
+                vibrate(Character.isUpperCase(c) ? 15 : 200);
+                if (Character.isUpperCase(c))
+                    next();
             });
         } else
             textView.setTextColor(getResources().getColor(R.color.white));
 
         return textView;
+    }
+
+    private void animateBackground(int colorId) {
+        TransitionDrawable transition = (TransitionDrawable) getResources().getDrawable(colorId);
+        root.setBackground(transition);
+        transition.startTransition(400);
+    }
+
+    private void vibrate(int vibrationDuration) {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(vibrationDuration, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            v.vibrate(vibrationDuration);
+        }
     }
 
 
